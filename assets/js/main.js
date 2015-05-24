dc0716f83f42b5a79770f759dda024a2eada2e07
Mike Bland <michael.bland@gsa.gov>
2015-05-24 14:16:02 -0400

    Add jQuery, as search.js depends on its selectors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
 require.config({
   paths: {
+    'jquery': 'vendor/jquery/jquery.min',
     'angular': 'vendor/angular/angular.min',
     'angularAMD': 'vendor/angularAMD/angularAMD.min',
     'liveSearch': 'vendor/angular-livesearch/liveSearch.min',
     'lunr': 'vendor/lunr.js/lunr.min'
   },
   shim: {
+    'angular': ['jquery'],
     'angularAMD': ['angular'],
     'liveSearch': ['angular'],
     'lunr': []
