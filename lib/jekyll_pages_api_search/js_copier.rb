b9ce8d2b9e4357d090441c403f6ab06de7b49936
Mike Bland <michael.bland@gsa.gov>
2015-06-23 10:08:37 -0400

    Push common code into JavascriptCopier

--- lib/jekyll_pages_api_search/js_copier.rb
+++ lib/jekyll_pages_api_search/js_copier.rb
@@ -4,15 +4,20 @@ require 'jekyll/static_file'
 
 module JekyllPagesApiSearch
   class JavascriptCopier
+    SOURCE = File.realpath(File.join(File.dirname(__FILE__), '..', '..'))
+    ASSETS_DIR = File.join('assets', 'js')
+    BEGIN_PATH = SOURCE.size + File::SEPARATOR.size
+
     def self.copy_to_site(site)
-      assets_path = File.join('assets', 'js')
-      source = File.realpath(File.join(File.dirname(__FILE__), '..', '..'))
-      begin_path = source.size + File::SEPARATOR.size
-      Dir[File.join(source, assets_path, '**', '*.js*')].each do |f|
-        next unless File.file? f
-        f = f[begin_path..-1]
+      self.search_bundle_paths do |f|
         site.static_files << ::Jekyll::StaticFile.new(
-          site, source, File.dirname(f), File.basename(f))
+          site, SOURCE, File.dirname(f), File.basename(f))
+      end
+    end
+
+    def self.search_bundle_paths
+      Dir.glob(File.join(SOURCE, ASSETS_DIR, 'search-bundle.js*')) do |f|
+        yield f[BEGIN_PATH..-1]
       end
     end
   end
