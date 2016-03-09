c6db447b875c017d6afdab07127b563c00940fd5
Mike Bland <mbland@acm.org>
2016-03-09 10:55:56 -0500

    Refactor SearchUi

--- assets/js/search-ui.js
+++ assets/js/search-ui.js
@@ -5,30 +5,30 @@ module.exports = SearchUi;
 // eslint-disable-next-line
 // based on https://github.com/angular/angular.js/blob/54ddca537/docs/app/src/search.js#L198-L206
 function SearchUi(doc, inputElement) {
-  var isForwardSlash = function(keyCode) {
-    return keyCode === 191;
-  };
-
-  var isInput = function(el) {
-    return el.tagName.toLowerCase() === 'input';
-  };
-
-  var giveSearchFocus = function() {
-    inputElement.focus();
-  };
-
-  var onKeyDown = function(event) {
-    if (isForwardSlash(event.keyCode) && !isInput(doc.activeElement)) {
-      event.stopPropagation();
-      event.preventDefault();
-      giveSearchFocus();
-    }
-  };
-
-  this.enableGlobalShortcut = function() {
-    doc.body.onkeydown = onKeyDown;
-  };
+  this.doc = doc;
+  this.inputElement = inputElement;
 }
 
 SearchUi.DEFAULT_SEARCH_INPUT_ID = 'search-input';
 SearchUi.DEFAULT_SEARCH_RESULTS_ID = 'search-results';
+
+function isForwardSlash(keyCode) {
+  return keyCode === 191;
+}
+
+function isInput(element) {
+  return element.tagName.toLowerCase() === 'input';
+}
+
+SearchUi.prototype.enableGlobalShortcut = function() {
+  var doc = this.doc,
+      inputElement = this.inputElement;
+
+  doc.body.onkeydown = function(event) {
+    if (isForwardSlash(event.keyCode) && !isInput(doc.activeElement)) {
+      event.stopPropagation();
+      event.preventDefault();
+      inputElement.focus();
+    }
+  };
+};
